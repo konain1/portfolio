d6bd7a8ef95f9e3fb771c5b29b7eb0146becfcfc
konain1 <barcafchd@gmail.com>
2022-05-15 17:14:27 +0530

    added js

---
+++ script.js
@@ -0,0 +1,18 @@
+const power = document.querySelector('.power');
+
+const section_one = document.querySelector('.section-one');
+
+
+
+
+
+
+
+
+
+power.addEventListener('click',()=>{
+    console.log("power on");
+    section_one.style.display="none";
+    
+    
+})
