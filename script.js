be34004220d13662663b4a23fd33592982a196ed
konain1 <barcafchd@gmail.com>
2022-05-15 20:01:32 +0530

    updated

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const power = document.querySelector('.power');
 
 const section_one = document.querySelector('.section-one');
 
+const section_two = document.querySelector('.section-2');
+
+const section_three = document.querySelector('.section-3');
+
+
+
+
 
 
 
@@ -13,6 +20,8 @@ const section_one = document.querySelector('.section-one');
 power.addEventListener('click',()=>{
     console.log("power on");
     section_one.style.display="none";
+    section_two.classList.remove('section-2');
+    section_three.classList.remove('section-3');
     
     
 })
